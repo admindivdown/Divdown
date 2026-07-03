9c55f023981607ed83552978991b0eb376faf75c
admindivdown <admindivdown@gmail.com>
2026-07-03 15:55:04 +0700

    Pembaruan penting

--- app.js
+++ app.js
@@ -140,10 +140,6 @@ document.body.appendChild(s);
 });
 // ===== END SOCIAL BAR =====
 
-// ===== PRELOAD BANNER MENU 320x50 =====
-document.addEventListener('DOMContentLoaded',()=>{setTimeout(()=>{const b=document.getElementById('menuBanner320');if(!b)return;const o=document.createElement('script');o.innerHTML="atOptions={'key':'a9e26a5898162babbd39410bf67794d2','format':'iframe','height':50,'width':320,'params':{}};";b.appendChild(o);const s=document.createElement('script');s.src='https://www.highperformanceformat.com/a9e26a5898162babbd39410bf67794d2/invoke.js';b.appendChild(s);},1500);});
-// ===== END PRELOAD BANNER MENU =====
-
 // ===== LOAD BANNER PROMO =====
 document.addEventListener('DOMContentLoaded',()=>{
 setTimeout(()=>{
