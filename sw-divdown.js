9e4fa1d13de936e60206b7ad3336d7b459f74c2d
admindivdown <admindivdown@gmail.com>
2026-07-29 11:47:45 +0000

    Pembaruan terbaru full final

--- sw-divdown.js
+++ sw-divdown.js
@@ -1,5 +1,5 @@
 /* ===== UPDATE CACHE VERSION ===== */
-const CACHE_VERSION = "7";
+const CACHE_VERSION = "8";
 const CACHE = `divdown-v${CACHE_VERSION}`;
 
 /* ===== END UPDATE CACHE VERSION ===== */ 
